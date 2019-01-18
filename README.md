92a899ba78599ac64630f53a628ad3ad2bf1659f
Ananth <agirijak@in.ibm.com>
2019-01-18 16:16:14 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,338 @@
-# bms-push-react-native
+
+# react-native-bmd-push-react
+
+IBM Cloud Mobile Services - Client SDK React Native for Push service
+
+This is the Push component of the React Native SDK for [IBM Cloud Mobile Services](https://console.ng.net/docs/mobile/index.html).
+
+
+## Requirements:
+
+Xcode 10+
+Android: minSdkVersion 16+, compileSdkVersion 28+
+React Native >= 0.57.8
+React Native cli >= 2.0.1
+
+## Installation
+
+Install the `bmd-push-react-native` using ,
+
+```JS
+$ npm install bmd-push-react-native --save
+```
+
+### Mostly automatic installation
+
+You can link the package like this,
+
+````JS
+$ react-native link bmd-push-react-native
+````
+
+### Manual installation
+
+If you want to link it manually ,
+
+#### iOS
+
+1. In XCode, in the project navigator, right click `Libraries` ➜ `Add Files to [your project's name]`
+2. Go to `node_modules` ➜ `bmd-push-react-native` and add `RNBmdPushReact.xcodeproj`
+3. In XCode, in the project navigator, select your project. Add `libRNBmdPushReact.a` to your project's `Build Phases` ➜ `Link Binary With Libraries`
+
+#### Android
+
+1. Open up `android/app/src/main/java/[...]/MainActivity.java`
+- Add `import com.bmdpush.react.RNBmdPushReactPackage;` to the imports at the top of the file
+- Add `new RNBmdPushReactPackage()` to the list returned by the `getPackages()` method
+2. Append the following lines to `android/settings.gradle`:
+```
+include ':bmd-push-react-native'
+project(':bmd-push-react-native').projectDir = new File(rootProject.projectDir,     '../node_modules/bmd-push-react-native/android')
+```
+3. Insert the following lines inside the dependencies block in `android/app/build.gradle`:
+```
+compile project(':bmd-push-react-native')
+```
+
+
+## Dependencies,
+
+### iOS
+
+#### Carthage 
+
+1. Create a cart file inside the iOS app folder. Carthage file should be like this ,
+
+```
+github "ibm-bluemix-mobile-services/bms-clientsdk-swift-push"
+```  
+
+2. Do the `carthage update` in terminal.
+
+3. open the Project in XCode and add the frameworks inside the `Embedded Binaries`
+
+#### Add dependencies framework directly .
+
+1.  Drag and drop the [BMSPush.framework](https://github.com/ibm-bluemix-mobile-services/bms-clientsdk-swift-push), [BMSCore.framework](https://github.com/ibm-bluemix-mobile-services/bms-clientsdk-swift-core) and [BMSAnalyticsAPI.framework](https://github.com/ibm-bluemix-mobile-services/bms-clientsdk-swift-analytics-api) to the iOS app.
+
+
+## Set up
+
+### iOS 
+
+Open the iOS app in XCode and do the following ,
+
+1. Under the Capabilities section enable the `Push Notifications` 
+2. Enable the `Background modes` for `Remote notifications` and `Background fetch`
+3. Go to `Build Settings` and make the following changes 
+
+a. locate `Other Linker Flags` and add `-lc++` , `-ObjC` and `$(inherited)`.
+b. locate `Framework Search Paths` and add `$(PROJECT_DIR)/Carthage/Build/iOS` as `non-recursive`.
+c. locate `Library Search Paths` and add `$(TOOLCHAIN_DIR)/usr/lib/swift/$(PLATFORM_NAME)`  as `non-recursive`.
+d. locate `Always Embed Swift Standard Libraries` and make it `Yes`.
+
+
+Now you can build and run the iOS app from Xcode or using the `react-native run-ios` command.
+
+
+### Android 
+
+Add the following inside the `AndroidManifest.xml` file .
+
+1. Add  `xmlns:tools="http://schemas.android.com/tools"` in the `<manifest ...> ` tag
+
+For example 
+```
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+xmlns:tools="http://schemas.android.com/tools" package="com.pushsample">
+```
+
+2. Add the following permissions,
+
+```
+<uses-permission android:name="android.permission.WAKE_LOCK" />
+<uses-permission android:name="android.permission.GET_ACCOUNTS" />
+<uses-permission android:name="android.permission.USE_CREDENTIALS" />
+<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+<uses-permission android:name="android.permission.ACCESS_WIFI_STATE"/>
+```
+
+3. Add `tools:replace="android:allowBackup"` inside the `<application ..>` tag
+
+For example 
+```
+<application
+android:name=".MainApplication"
+android:label="@string/app_name"
+android:icon="@mipmap/ic_launcher"
+android:allowBackup="false"
+android:launchMode="singleTask"
+android:theme="@style/AppTheme"
+tools:replace="android:allowBackup">
+```
+
+4. Add the following inside the `<activity android:name=".MainActivity" ....>`,
+
+```
+<intent-filter>
+<action android:name="yourapp.bundle.IBMPushNotification" />
+<category android:name="android.intent.category.DEFAULT" />
+</intent-filter>
+```
+
+5. Add the following lines,
+
+```
+<activity android:name="com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushNotificationHandler" android:theme="@android:style/Theme.NoDisplay"/>
+
+<service android:exported="true" android:name="com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushIntentService">
+<intent-filter>
+<action android:name="com.google.firebase.MESSAGING_EVENT"/>
+</intent-filter>
+</service>
+<service android:exported="true" android:name="com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPush">
+<intent-filter>
+<action android:name="com.google.firebase.INSTANCE_ID_EVENT"/>
+</intent-filter>
+</service>
+```
+
+Now you can build and run the android app from android studio or using the `react-native run-android` command.
+
+
+## Usage
+
+### Initialization
+
+Import the following dependecnice ,
+
+```JS
+import {Push} from 'react-native-bmd-push-react';
+import { DeviceEventEmitter } from 'react-native';
+```
+
+To initialize Push use the following code 
+
+```JS
+
+// Initialize for push notifications without passing options
+Push.init({
+"appGUID":"xxxxxx-xxxx-41xxxx67-xxxxx-xxxxx",
+"clientSecret":"xxxxx-xxxxx-xxxx-xxxxx-xxxxxxx",
+"region":".ng.bluemix.net"
+}).then(function(response) {
+
+alert("InitSuccess: " + response);
+
+}).catch(function(e) {
+
+alert("Init Error: " + e);
+
+});
+
+// Initialize for push notifications with passing options
+// Initialize for iOS actionable push notifications, custom deviceId and varibales for Parameterize Push Notifications 
+
+var optionsJson = {"categories": { "Category_Name1":[
+{
+"IdentifierName":"IdentifierName_1",
+"actionName":"actionName_1",
+"IconName":"IconName_1"
+},
+{
+"IdentifierName":"IdentifierName_2",
+"actionName":"actionName_2",
+"IconName":"IconName_2"
+}
+]},
+"deviceId":"mydeviceId",
+"variables":{"username":"ananth","accountNumber":"536475869765475869"}
+};
+
+Push.init({
+"appGUID":"xxxxxx-xxxx-41xxxx67-xxxxx-xxxxx",
+"clientSecret":"xxxxx-xxxxx-xxxx-xxxxx-xxxxxxx",
+"region":".ng.bluemix.net",
+"options": optionsJson
+}).then(function(response) {
+
+alert("Init Success: " + response);
+
+}).catch(function(e) {
+
+alert("Init Error: " + e);
+});
+
+```
+
+**IMPORTANT: These are the following supported `regions` - `".ng.bluemix.net", ".eu-gb.bluemix.net" , ".au-syd.bluemix.net", ".eu-de.bluemix.net" and ".us-east.bluemix.net"`
+
+
+### Register for push 
+
+```JS
+
+// Register device for push notification without UserId
+var options = {};
+Push.register(options).then(function(response) {
+alert("Success: " + response);
+}).catch(function(e) {
+alert("Register Error: " + e);
+});
+
+// Register device for push notification with UserId
+
+var options = {"userId":"ananthreact"};
+Push.register(options).then(function(response) {
+alert("Success: " + response);
+}).catch(function(e) {
+alert("Register Error: " + e);
+});
+```
+
+### UnRegister from push 
+
+```JS
+push.unregisterDevice().then(function(response) {
+alert("Success unregisterDevice : " + response);
+}).catch(function(e) {
+alert("UnRegister error : " + e);
+});
+```
+### Retrieve Available Tags
+
+```JS
+Push.retrieveAvailableTags().then(function(response) {
+alert("get tags : " + response);
+}).catch(function(e) {
+alert("get tags error : " + e);
+});
+```
+
+### Subscribe to a tag
+
+```JS
+Push.subscribe(response[0]).then(function(response) {
+alert("subscribe tags : " + response);
+}).catch(function(e) {
+alert("subscribe tags error : " + e);
+});
+```
+
+### Retrieve Subscriptions
+
+```JS
+
+Push.retrieveSubscriptions().then(function(response) {
+alert("retrieveSubscriptions tags : " + response);
+}).catch(function(e){
+alert("error retrieveSubscriptions : " + e);
+});
+```
+
+### Unsubscribe from tag 
+
+```JS
+var tag = "tag1";
+Push.unsubscribe(tag).then(function(response) {
+alert("unsubscribe tag : " + response);
+}).catch(function(e) {
+alert("Error : " + e);
+});
+```
+
+
+
+### Samples & videos
+
+* Please visit for samples - [Github Sample](https://github.com/ibm-bluemix-push-notifications/bms-samples-react-native-hellopush)
+
+### Learning More
+
+* Visit the **[IBM Cloud Developers Community](https://developer.ibm.com/bluemix/)**.
+
+* [Getting started with IBM MobileFirst Platform for iOS](https://www.ng.bluemix.net/docs/mobile/index.html)
+
+### Connect with IBM Cloud
+
+[Twitter](https://twitter.com/ibmbluemix) |
+[YouTube](https://www.youtube.com/playlist?list=PLzpeuWUENMK2d3L5qCITo2GQEt-7r0oqm) |
+[Blog](https://developer.ibm.com/bluemix/blog/) |
+[Facebook](https://www.facebook.com/ibmbluemix) |
+[Meetup](http://www.meetup.com/bluemix/)
+
+
+=======================
+Copyright 2016 IBM Corp.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ ios/BMSPushReactBinder.swift
@@ -0,0 +1,415 @@
+//
+//  BMSPushReactBinder.swift
+//  RNBmdPushReact
+//
+//  Created by Anantha Krishnan K G on 17/01/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
+import BMSPush
+import BMSCore
+import BMSAnalyticsAPI
+import UIKit
+
+#if swift(>=3.0)
+import UserNotifications
+import UserNotificationsUI
+#endif
+
+@objc public class BMSPushReactBinder: NSObject, BMSPushObserver {
+    
+    // MARK: Constants
+    private let DEVICEID = "deviceId";
+    private let CATEGORIES = "categories";
+    private let USERID = "userId";
+    private let IDENTIFIER_NAME = "IdentifierName";
+    private let ACTION_NAME = "actionName";
+    private let ICON_NAME = "IconName";
+    private let PUSHVARIABLES = "variables";
+    
+    private let PUSHAPPGUID = "appGUID";
+    private let PUSHCLIENTSECRET = "clientSecret";
+    private let PUSHREGION = "region";
+    private let PUSHOPTIONS = "options";
+    
+    // MARK: VARIBALES
+    @objc public static let sharedInstance = BMSPushReactBinder()
+    private var push:BMSPushClient?
+    
+    var pushUserId = String();
+    var shouldRegister:Bool = false
+    
+    #if swift(>=3.0)
+    var bmsPushToken:Data?
+    #else
+    var bmsPushToken:NSData?
+    #endif
+    
+    var registerPromiseResolve:RCTPromiseResolveBlock?
+    var registerPromiseReject:RCTPromiseRejectBlock?
+    
+    var initPromiseResolve:RCTPromiseResolveBlock?
+    var initPromiseReject:RCTPromiseRejectBlock?
+    
+    // MARK: FUNCTIONS
+    public func onChangePermission(status: Bool) {
+        
+        print("Push Notification is enabled:  \(status)" as NSString)
+        if status {
+            
+            if(initPromiseResolve != nil) {
+                initPromiseResolve!("IBM cloud Push Notification init successful")
+            }
+        } else {
+            if(initPromiseReject != nil) {
+                initPromiseReject!("400","IBM cloud Push Notification init failed",nil);
+            }
+        }
+    }
+    
+    private override init() {
+        super.init()
+        push =  BMSPushClient.sharedInstance
+        push?.delegate = self
+        print("BMSPushReactBinder init")
+        
+        // NOTIFICATION OBSERVERS FOR APPDELEGATE METHODS
+        NotificationCenter.default.addObserver(self, selector: #selector(BMSPushReactBinder.didRegisterForRemoteNotifications(notification:)), name: NSNotification.Name(rawValue: REACTBMDPushDidRegisterForRemoteNotificationsWithDeviceToken), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(BMSPushReactBinder.didFailToRegisterForRemoteNotifications(notification:)), name: NSNotification.Name(rawValue: REACTBMDushDidFailToRegisterForRemoteNotificationsWithError), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(BMSPushReactBinder.didReceiveRemoteNotification(notification:)), name: NSNotification.Name(rawValue: REACTBMDPushDidReceiveRemoteNotifications), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(BMSPushReactBinder.didReceiveRemoteNotification(notification:)), name: NSNotification.Name(rawValue: REACTBMDPushHandleActionWithIdentifier), object: nil)
+    }
+    
+    
+    @objc public func initializeWithAppGUID (config: NSDictionary, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) -> Void {
+        
+        self.initPromiseResolve = resolve
+        self.initPromiseReject = reject
+        if config.count <= 0 {
+            print("Empty config in init")
+            let message = "Empty config in init"
+            reject("\(404)", message, nil)
+            return
+        }
+        
+        guard let appGUID = config.value(forKey: PUSHAPPGUID) as? String else {
+            print("Empty appGUID in init")
+            let message = "Empty appGUID in init"
+            reject("\(404)", message, nil)
+            return
+        }
+        
+        guard let clientSecret = config.value(forKey: PUSHCLIENTSECRET)as? String else {
+            print("Empty clientSecret in init")
+            let message = "Empty clientSecret in init"
+            reject("\(404)", message, nil)
+            return
+        }
+        
+        guard let region = config.value(forKey: PUSHREGION) as? String else {
+            print("Empty region in init")
+            let message = "Empty region in init"
+            reject("\(404)", message, nil)
+            return
+        }
+        
+        BMSClient.sharedInstance.initialize(bluemixRegion: region)
+        
+        if let bmsNotifOptions = config.value(forKey: PUSHOPTIONS) as? NSDictionary  {
+            
+            if bmsNotifOptions.count > 0 {
+                
+                var categoryArray = [BMSPushNotificationActionCategory]()
+                let notifOptions = BMSPushClientOptions();
+                
+                if bmsNotifOptions[CATEGORIES] != nil {
+                    guard let result = bmsNotifOptions.value(forKey:CATEGORIES) as? [String:AnyObject] else {
+                        print("init error: Invalid BMSPush Options")
+                        let message = "init error: Invalid BMSPush Options"
+                        reject("\(404)", message, nil)
+                        return
+                    }
+                    if(result.count > 0){
+                        for name in result{
+                            
+                            let identifiers:NSArray = (name.value) as! NSArray
+                            var actionArray = [BMSPushNotificationAction]()
+                            for identifier in identifiers {
+                                
+                                let resultJson = identifier as? NSDictionary
+                                let identifierName = resultJson?.value(forKey: IDENTIFIER_NAME);
+                                let actionName = resultJson?.value(forKey: ACTION_NAME);
+                                
+                                actionArray.append(BMSPushNotificationAction(identifierName: identifierName as! String, buttonTitle:actionName as! String, isAuthenticationRequired: false, defineActivationMode: UIUserNotificationActivationMode.foreground))
+                            }
+                            let category = BMSPushNotificationActionCategory(identifierName: name.key , buttonActions: actionArray)
+                            categoryArray.append(category)
+                        }
+                        notifOptions.setInteractiveNotificationCategories(categoryName: categoryArray)
+                    }
+                }
+                
+                if bmsNotifOptions[DEVICEID] != nil {
+                    guard let deviceId = bmsNotifOptions.value(forKey:DEVICEID) as? String else {
+                        print("init error: Invalid BMSPush Options")
+                        let message = "init error: Invalid BMSPush Options"
+                        reject("\(404)", message, nil)
+                        return
+                    }
+                    notifOptions.setDeviceId(deviceId: deviceId)
+                }
+                if bmsNotifOptions[PUSHVARIABLES] != nil {
+                    guard let variables = bmsNotifOptions.value(forKey:PUSHVARIABLES) as? [String: String] else {
+                        print("init error: Invalid BMSPush Options")
+                        let message = "init error: Invalid BMSPush Options"
+                        reject("\(404)", message, nil)
+                        return
+                    }
+                    notifOptions.setPushVariables(pushVariables: variables)
+                }
+                let push = BMSPushClient.sharedInstance;
+                push.initializeWithAppGUID(appGUID: appGUID, clientSecret: clientSecret, options: notifOptions);
+            } else {
+                print("init error: Invalid BMSPush Options")
+                let message = "init error: Invalid BMSPush Options"
+                reject("\(404)", message, nil)
+                return
+            }
+        } else {
+            
+            push?.initializeWithAppGUID(appGUID: appGUID, clientSecret: clientSecret)
+        }
+    }
+    
+    @objc public func registerDevice(_ config: NSDictionary,
+                                     resolver resolve: @escaping RCTPromiseResolveBlock,
+                                     rejecter reject: @escaping RCTPromiseRejectBlock) -> Void {
+        if let userId = config.value(forKey: USERID) as? String{
+            self.pushUserId = userId
+        }
+        self.registerPromiseResolve = resolve
+        self.registerPromiseReject = reject
+        shouldRegister = true;
+        registerforPush()
+    }
+    
+    @objc public func unregisterDevice(_ resolve: @escaping RCTPromiseResolveBlock,
+                                       rejecter reject: @escaping RCTPromiseRejectBlock) -> Void {
+        
+        push?.unregisterDevice(completionHandler: { (response, statusCode, error) in
+            
+            if error.isEmpty {
+                
+                let message = response?.description
+                UIApplication.shared.unregisterForRemoteNotifications()
+                resolve(message);
+            } else {
+                
+                let message = error.description
+                reject("\(statusCode ?? 404)",message,error as? Error)
+            }
+        })
+        
+    }
+    
+    @objc public func retrieveSubscriptions(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) -> Void {
+        
+        push?.retrieveSubscriptionsWithCompletionHandler { (response, statusCode, error) -> Void in
+            
+            if error.isEmpty {
+                let message = response?.description
+                resolve(message);
+            }
+            else {
+                
+                print( "Error during retrieveSubscriptions \(error) ")
+                let message = error.description
+                reject("\(statusCode ?? 404)", message, error as? Error)
+            }
+        }
+        
+    }
+    
+    @objc public func retrieveAvailableTags(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) -> Void {
+        
+        push?.retrieveAvailableTagsWithCompletionHandler(completionHandler: { (response, statusCode, error) in
+            
+            if error.isEmpty {
+                
+                let message = response?.description
+                resolve(message);
+                
+            }
+            else{
+                
+                print( "Error during retrieveAvailableTags \(error) ")
+                let message = error.description
+                reject("\(statusCode ?? 404)", message, error as? Error)
+            }
+        })
+        
+    }
+    
+    
+    @objc public func subscribe(_ tag: String , resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) -> Void {
+        
+        if tag.isEmpty {
+            
+            print( "Error during subscribe: empty tag")
+            let message = "Error during subscribe: empty tag"
+            reject("\(404)", message, nil)
+        } else {
+            
+            let tagsArray = [tag]
+            push?.subscribeToTags(tagsArray: tagsArray as NSArray, completionHandler: { (response, statusCode, error) -> Void in
+                
+                if error.isEmpty {
+                    
+                    let message = response?.description
+                    resolve(message);
+                    
+                    
+                }else{
+                    
+                    print( "Error during subscribe Tags \(error) ")
+                    let message = error.description
+                    reject("\(statusCode ?? 404)", message, error as? Error)
+                }
+            });
+        }
+    }
+    
+    
+    @objc public func unsubscribe(_ tag: String , resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) -> Void {
+        
+        if tag.isEmpty {
+            
+            let message = "Error during unsubscribe: empty tag"
+            print( message)
+            reject("\(404)", message, nil)
+        } else {
+            
+            let tagsArray = [tag]
+            push?.unsubscribeFromTags(tagsArray: tagsArray as NSArray, completionHandler: { (response, statusCode, error) -> Void in
+                
+                if error.isEmpty {
+                    
+                    let message = response?.description
+                    resolve(message);
+                    
+                }else{
+                    
+                    print( "Error during unsubscribe Tags \(error) ")
+                    let message = error.description
+                    reject("\(statusCode ?? 404)", message, error as? Error)
+                }
+            });
+        }
+    }
+    
+    @objc func didFailToRegisterForRemoteNotifications(notification:Notification) {
+        
+        
+        guard let error = notification.object as? Error else {
+            if self.registerPromiseReject != nil {
+                self.registerPromiseReject!("\(404)","didFailToRegisterForRemoteNotifications",nil)
+            }
+            return
+        }
+        if self.registerPromiseReject != nil {
+            self.registerPromiseReject!("\(404)","didFailToRegisterForRemoteNotifications",error)
+        }
+    }
+    
+    @objc func didRegisterForRemoteNotifications(notification:Notification) {
+        
+        guard let token = notification.object as? Data else {
+            print("Error wile getting token")
+            return
+        }
+        bmsPushToken = token
+        registerforPush()
+    }
+    
+    func registerforPush() {
+        
+        if let token = bmsPushToken , shouldRegister {
+            
+            if pushUserId.isEmpty {
+                
+                push?.registerWithDeviceToken(deviceToken: token, completionHandler: { (response, statusCode, error) in
+                    
+                    if (!error.isEmpty) {
+                        let message = error.description
+                        // call error callback
+                        if self.registerPromiseReject != nil {
+                            self.registerPromiseReject!("\(statusCode ?? 404)",message,error as? Error)
+                        }
+                    }
+                    else {
+                        if self.registerPromiseResolve != nil {
+                            self.registerPromiseResolve!(response)
+                        }
+                    }
+                })
+            } else {
+                
+                push?.registerWithDeviceToken(deviceToken: token, WithUserId: self.pushUserId, completionHandler: { (response, statusCode, error) in
+                    if (!error.isEmpty) {
+                        let message = error.description
+                        // call error callback
+                        if self.registerPromiseReject != nil {
+                            self.registerPromiseReject!("\(statusCode ?? 404)",message,error as? Error)
+                        }
+                    }
+                    else {
+                        if self.registerPromiseResolve != nil {
+                            self.registerPromiseResolve!(response)
+                        }
+                    }
+                })
+            }
+        } else {
+            
+            print("Error in token")
+        }
+    }
+    
+    
+    @objc func didReceiveRemoteNotification(notification:Notification) {
+        
+        print("ha haha 7888")
+        guard let userinfo = notification.userInfo else {
+            return
+        }
+        
+        var notif: [String : AnyObject] = [:]
+        
+        if let _ = userinfo["has-template"] as? Int {
+            BMSPushClient.sharedInstance.didReciveBMSPushNotification(userInfo: userinfo) { (res, error) in
+                return
+            }
+        } else {
+            
+            notif["message"] = ((userinfo["aps"] as! NSDictionary).value(forKey: "alert") as! NSDictionary).value(forKey: "body") as AnyObject?
+            
+            notif["payload"] = userinfo["payload"] as AnyObject?
+            notif["url"] = userinfo["url"] as AnyObject?
+            notif["sound"] = (userinfo["aps"] as! NSDictionary).value(forKey: "sound") as AnyObject?
+            notif["badge"] = (userinfo["aps"] as! NSDictionary).value(forKey: "badge") as AnyObject?
+            
+            notif["action-loc-key"] = ((userinfo["aps"] as! NSDictionary).value(forKey: "alert") as! NSDictionary).value(forKey: "action-loc-key") as AnyObject?
+            
+            if let actionName = userinfo["identifierName"] {
+                notif["identifierName"] = actionName as AnyObject
+            }
+            
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: REACTBMDPushSendPushNotifications) , object: nil, userInfo: notif)
+        }
+    }
+    
+}
